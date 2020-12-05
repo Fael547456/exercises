2e4dc4a2dd2211b28ca5a51b10305e89f1e92c81
Fael547456 <75342234+Fael547456@users.noreply.github.com>
2020-12-05 11:56:27 -0300

    Update 3-Soma dos múltiplos de 3 e 5.php

--- 3-Soma dos múltiplos de 3 e 5.php
+++ 3-Soma dos múltiplos de 3 e 5.php
@@ -2,13 +2,12 @@
 
 class Multiplos_3_5
  {
-  public $somaXY;
-  
+  public $Soma;
   public function __construct($limite)
   {
    for($i=0; $i<$limite; $i++){
     if($i % 3 == 0 || $i % 5 == 0){
-      $this->somaXY += $i; //<-Soma o total dos multiplos de 3 e 5
+      $this->Soma += $i; //<-Soma o total dos multiplos de 3 e 5
      }
     }
    }
@@ -16,6 +15,6 @@ class Multiplos_3_5
  
    $limite = 15; //<-Limite superior de 15
    $mycalc = new Multiplos_3_5($limite); //<-Cria o objeto com a variavel limite como paramêtro 
-   echo "A soma dos multiplos de 3 e 5 dentro do limite de ".$limite." números é: ".$mycalc->somaXY;
+   echo "A soma dos multiplos de 3 e 5 dentro do limite de ".$limite." números é: ".$mycalc->Soma;
 
 ?> 
