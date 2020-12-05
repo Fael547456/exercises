ba6a41e16f85782d277f1a5d8beaa5f11e14aa83
Fael547456 <75342234+Fael547456@users.noreply.github.com>
2020-12-05 10:04:58 -0300

    Update 3-Soma dos múltiplos de 3 e 5.php

--- 3-Soma dos múltiplos de 3 e 5.php
+++ 3-Soma dos múltiplos de 3 e 5.php
@@ -1,25 +1,33 @@
 <?php
 
-    $X = 3;
-    $Y = 5;
-    $somaX  = 0;
-    $somaY  = 0;
-    $somaXY = 0;
-    $limite = 15;
-
-    for($i = 1; $i < $limite; $i++) {
-        if($i % $X == 0) {
-            $somaX = $i + $somaX;
+class Multiplos_3_5
+{
+  public $X = 3;
+  public $Y = 5;
+  public $somaX;
+  public $somaY;
+  public $somaXY;
+  
+  public function __construct($limite)
+  {
+   for($i = 1; $i < $limite; $i++) {
+        if($i % $this->X == 0) {
+            $this->somaX = $i + $this->somaX;
         }
-    }
-    for($i = 1; $i < $limite; $i++) {
-        if($i % $Y == 0) {
-            $somaY = $i + $somaY;
+      }
+	
+   for($i = 1; $i < $limite; $i++) {
+        if($i % $this->Y == 0) {
+            $this->somaY = $i + $this->somaY;
         }
-    }
-    
-	$somaXY = $somaX + $somaY;
-    
-	echo "A soma dos multiplos de ".$X." e ".$Y." até o limite de ".$limite." numeros é: ".$somaXY;
+      }	
+	 
+	  $this->somaXY = $this->somaX + $this->somaY;
+   }
+}
+ 
+   $lim = 15; //<-Limite superior de 15
+   $mycalc = new Multiplos_3_5($lim); //<-Cria o objeto com a variavel limite como paramêtro 
+   echo "A soma dos multiplos de 3 e 5 dentro do limite de ".$lim." numeros é: ".$mycalc->somaXY;
 
 ?> 
