7bc175112e6bac41ae12a881b10129bc97160220
Fael547456 <75342234+Fael547456@users.noreply.github.com>
2020-12-05 11:54:28 -0300

    Update 3-Soma dos múltiplos de 3 e 5.php

--- 3-Soma dos múltiplos de 3 e 5.php
+++ 3-Soma dos múltiplos de 3 e 5.php
@@ -1,31 +1,21 @@
 <?php
 
 class Multiplos_3_5
-{
+ {
   public $somaXY;
   
   public function __construct($limite)
   {
-   $somaX = 0;
-   $somaY = 0; 
-   for($i = 1; $i < $limite; $i++) {
-        if($i % 3 == 0) { 
-           $somaX = $i + $somaX; //<-Soma multiplos de 3
-        }
-      }
-	
-   for($i = 1; $i < $limite; $i++) {
-        if($i % 5 == 0) {
-            $somaY = $i + $somaY; //<-Soma multiplos de 5
-        }
-      }	
-	 
-	  $this->somaXY = $somaX + $somaY; //<-Soma o total dos multiplos de 3 e 5
+   for($i=0; $i<$limite; $i++){
+    if($i % 3 == 0 || $i % 5 == 0){
+      $this->somaXY += $i; //<-Soma o total dos multiplos de 3 e 5
+     }
+    }
    }
-}
+  } 
  
-   $lim = 15; //<-Limite superior de 15
-   $mycalc = new Multiplos_3_5($lim); //<-Cria o objeto com a variavel limite como paramêtro 
-   echo "A soma dos multiplos de 3 e 5 dentro do limite de ".$lim." numeros é: ".$mycalc->somaXY;
+   $limite = 15; //<-Limite superior de 15
+   $mycalc = new Multiplos_3_5($limite); //<-Cria o objeto com a variavel limite como paramêtro 
+   echo "A soma dos multiplos de 3 e 5 dentro do limite de ".$limite." números é: ".$mycalc->somaXY;
 
 ?> 
